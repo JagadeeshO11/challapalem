e051791bd8a4a60be9edfd9238af3e97eac91aa5
OsuruJagadeesh <157037205+JagadeeshO11@users.noreply.github.com>
2026-09-04 19:00:16 +0530

    Connect business detail to content service

--- app/businesses/[id].jsx
+++ app/businesses/[id].jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import DynamicDetail from '../../src/components/DynamicDetail';
-import { businesses } from '../../src/data/content';
 
 export default function BusinessDetailScreen() {
-  return <DynamicDetail collection={businesses} eyebrow="BUSINESS" />;
+  return <DynamicDetail type="business" eyebrow="BUSINESS" />;
 }
