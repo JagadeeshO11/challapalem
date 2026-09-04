930bbec0f6b3ccddfdd5bb26277cb3f6feb94f82
OsuruJagadeesh <157037205+JagadeeshO11@users.noreply.github.com>
2026-09-04 19:00:08 +0530

    Connect event detail to content service

--- app/events/[id].jsx
+++ app/events/[id].jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import DynamicDetail from '../../src/components/DynamicDetail';
-import { events } from '../../src/data/content';
 
 export default function EventDetailScreen() {
-  return <DynamicDetail collection={events} eyebrow="EVENT" />;
+  return <DynamicDetail type="event" eyebrow="EVENT" />;
 }
