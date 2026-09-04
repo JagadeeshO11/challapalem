d7bad7267dc5c2bf79f407862952a11da184133f
OsuruJagadeesh <157037205+JagadeeshO11@users.noreply.github.com>
2026-09-04 19:00:22 +0530

    Connect community detail to content service

--- app/community/[id].jsx
+++ app/community/[id].jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import DynamicDetail from '../../src/components/DynamicDetail';
-import { community as communityItems } from '../../src/data/content';
 
 export default function CommunityDetailScreen() {
-  return <DynamicDetail collection={communityItems} eyebrow="COMMUNITY" />;
+  return <DynamicDetail type="community" eyebrow="COMMUNITY" />;
 }
