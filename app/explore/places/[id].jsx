b6cd6d1b09562f342abb212b7cb1877c71b97d71
OsuruJagadeesh <157037205+JagadeeshO11@users.noreply.github.com>
2026-09-04 19:00:02 +0530

    Connect place detail to content service

--- app/explore/places/[id].jsx
+++ app/explore/places/[id].jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import DynamicDetail from '../../../src/components/DynamicDetail';
-import { places } from '../../../src/data/content';
 
 export default function PlaceDetailScreen() {
-  return <DynamicDetail collection={places} eyebrow="PLACE" />;
+  return <DynamicDetail type="place" eyebrow="PLACE" />;
 }
